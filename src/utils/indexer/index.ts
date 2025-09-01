e5701db639c5d70715a1920f41221b3d272a4b21
John Huang <johnqh@yahoo.com>
2025-08-31 17:12:19 -0700

    Modified global git ignore

---
+++ src/utils/indexer/index.ts
@@ -0,0 +1,7 @@
+/**
+ * Indexer utilities - Platform-agnostic indexer helpers
+ * These will be implemented with proper DI when moved from main project
+ */
+
+// Placeholder exports to make the build pass
+// TODO: Implement actual indexer utilities with proper DI
